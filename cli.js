e5fd262c0d90b4ce50bb2a5cb182509fcc2554a1
estrattonbailey <ericstrattonbailey@gmail.com>
2019-09-13 10:09:51 -0400

    fix cli version output

--- cli.js
+++ cli.js
@@ -3,22 +3,17 @@
 
 const fs = require('fs-extra')
 const path = require('path')
+const pkg = require('./package.json')
 
 const cwd = process.cwd()
 const args = process.argv.slice(2)
+
+const index = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8')
 const out = path.join(
   path.join(cwd, args[0]),
   'index.html'
 )
 
-fs.copy(
-  path.resolve(__dirname, './index.html'),
-  out,
-  e => {
-    if (e) {
-      throw e
-    }
+fs.outputFileSync(out, index.replace('VERSION', pkg.version))
 
-    console.log(`picosite created in ${out}`)
-  }
-)
+console.log(`picosite created in ${out}`)
